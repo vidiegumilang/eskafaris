860c13da6d40d0bd8a849318e682b8cf6dee0c4b
vidiegumilang <vidiegumilang@ppicurug.ac.id>
2026-04-06 17:10:33 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const flightData = [
+  { flight: "GA123", destination: "Jakarta (CGK)", time: "08:00", status: "Departed", gate: "G1" },
+  { flight: "SQ456", destination: "Singapore (SIN)", time: "09:30", status: "On Time", gate: "A5" },
+  { flight: "EK789", destination: "Dubai (DXB)", time: "11:15", status: "Delayed", gate: "B2" }
+];
